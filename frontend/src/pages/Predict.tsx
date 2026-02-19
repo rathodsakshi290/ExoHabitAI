71241f0a3f2aff51e4560fbead13a7568e7a6339
Sakshi Rathod <rathodsakshi290@gmail.com>
2026-02-19 00:57:10 -0800

    Update Predict.tsx

--- frontend/src/pages/Predict.tsx
+++ frontend/src/pages/Predict.tsx
@@ -57,7 +57,7 @@ const Predict = () => {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://exohabitai-jynx.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
