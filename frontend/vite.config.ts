03b4aae435a821755790bc89be7bb84062b78fef
Sakshi Rathod <rathodsakshi290@gmail.com>
2026-02-19 09:32:14 -0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,9 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      "/predict": "http://localhost:5000",
-    },
+  build: {
+    outDir: "dist",
   },
 });
